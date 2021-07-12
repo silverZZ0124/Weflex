7e5e78a25238bf80b3f1d36b127434a2cb126950
5hasis <dhgkdud4002@naver.com>
2021-07-12 16:27:38 +0900

    새로운 장르 입력

--- kh7finalteam1/src/main/java/com/kh/finalteam1/controller/AdminViewController.java
+++ kh7finalteam1/src/main/java/com/kh/finalteam1/controller/AdminViewController.java
@@ -1,7 +1,12 @@
 package com.kh.finalteam1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.finalteam1.repository.GenreDao;
 
 @Controller
 @RequestMapping("/admin")
@@ -11,5 +16,19 @@ public class AdminViewController {
 	@RequestMapping("/")
 	public String index() {
 		return "admin/index";
+	}
+	
+	@GetMapping("/genre")
+	public String genre() {
+		return "admin/genre";
+	}
+	
+	@Autowired
+	private GenreDao genreDao;
+	
+	@GetMapping("/insertGenre")
+	public String insertGenre(@RequestParam String genreName) {
+		genreDao.insert(genreName);
+		return "redirect:admin/index";
 	}
 }
