24af3793ee9140ea366b084728622c3c4db079c8
thrudtn <so960414@naver.com>
2021-07-19 09:47:32 +0900

    회원가입 1~3페이지

--- kh7finalteam1/src/main/java/com/kh/finalteam1/controller/LoginController.java
+++ kh7finalteam1/src/main/java/com/kh/finalteam1/controller/LoginController.java
@@ -9,4 +9,32 @@ public class LoginController {
 public String login() {
 	return "login/loginstart"; 
 }
+@Controller
+public class Join1Controller{
+	@GetMapping("/join1")
+	public String login() {
+		return "login/join1";
+	}
+}
+@Controller
+public class Join2Controller{
+	@GetMapping("/join2")
+	public String login() {
+		return "login/join2";
+	}
+}
+@Controller
+public class Join3Controller{
+	@GetMapping("join3")
+	public String login() {
+		return "login/join3";
+	}
+}
+@Controller
+public class Join4Controller{
+	@GetMapping("join4")
+	public String login() {
+		return "login/join4";
+	}
+}
 }
