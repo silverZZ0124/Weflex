03a46f05c59c4cd90864f1dba62def34490b0533
parkminwoong <alsdnd420@naver.com>
2021-07-28 18:59:07 +0900

    월별 가입 회원 및 구매 건수 조회 VO

---
+++ kh7finalteam1/src/main/java/com/kh/finalteam1/vo/ClientJoinBuyCountVO.java
@@ -0,0 +1,22 @@
+package com.kh.finalteam1.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class ClientJoinBuyCountVO {
+	private int jan;
+	private int feb;
+	private int mar;
+	private int apr;
+	private int may;
+	private int jun;
+	private int jul;
+	private int aug;
+	private int sep;
+	private int oct;
+	private int nov;
+	private int dec;
+}
