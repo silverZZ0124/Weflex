9e988fd1a7c34ac04dc03ecaa54f3ffd07efdbd8
seiyun0809 <qweasdzxc809@naver.com>
2021-08-02 16:49:45 +0900

    ajax 대응 date타입

--- kh7finalteam1/src/main/java/com/kh/finalteam1/vo/NoticeVo.java
+++ kh7finalteam1/src/main/java/com/kh/finalteam1/vo/NoticeVo.java
@@ -2,6 +2,7 @@ package com.kh.finalteam1.vo;
 
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +14,8 @@ public class NoticeVo {
 	private int noticeNo;
 	private int clientNo;
 	private String noticeName;
-	private String noticeWrite; 
+	private String noticeWrite;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date noticeDate;
 	private int noticeViewCount;
 	private String clientName;
