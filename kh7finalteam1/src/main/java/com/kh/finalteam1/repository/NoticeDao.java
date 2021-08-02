f7d0bd1239fbf5dd094c2e1f60475fb649c4f083
seiyun0809 <qweasdzxc809@naver.com>
2021-08-02 17:21:10 +0900

    repository

--- kh7finalteam1/src/main/java/com/kh/finalteam1/repository/NoticeDao.java
+++ kh7finalteam1/src/main/java/com/kh/finalteam1/repository/NoticeDao.java
@@ -12,5 +12,11 @@ public interface NoticeDao {
 	
 	boolean noticeDelete(int noticeNo);
 	
+	NoticeVo noticeGet(int noticeNo);
+	
 	void noticeInsert(NoticeDto noticeDto);
+	
+	void noticeUpdate(NoticeDto noticeDto);
+	
+	void noticeCount(int noticeNo);
 }
